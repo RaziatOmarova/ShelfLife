c27ea9f95a7990b507787798c7816fbb163ea9ca
Raziat <114012022+RaziatOmarova@users.noreply.github.com>
2022-11-18 20:31:00 +0300

    Добавьте файлы проекта.

---
+++ Shelf life/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelf_life
+{
+    internal class Products
+    {
+        public Products(string name,int expirationdate, int time)
+        {
+            this.Name = name;
+            this.Expirationdate = expirationdate;
+            this.Time = time;
+        }
+        public string Name { get; set; }
+        public int Expirationdate { get; set; }
+        public int Time { get; set; }
+    }
+}
---
+++ Shelf life/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shelf_life
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> product = new List<Products>();
+            {
+                new Products("Milk",10,14);
+                new Products("Cheese",20,17);
+                new Products("Bread",4,10);
+                new Products("Tomatoes",15,19);
+                new Products("Cucumbers",15,18);
+            }
+            var time = product.Where(item => item.Time >= item.Expirationdate).ToList();
+            foreach (var item in time)
+            {
+                Console.WriteLine($"Просроченные продукты: {item.Name}");
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
